c1dc2a8e041a9ef0b3f93b9bc1278798f36771a8
Steffen Hiller <s.hiller@caprireleases.com>
2011-08-11 23:05:28 -0430

    Init: Scroll field to top test.

---
+++ index.js
@@ -0,0 +1,38 @@
+Ext.regApplication({
+    name: 'App',
+    launch: function() {
+        var items = [],
+            fieldCfg,
+            placeHolder;
+            
+        fieldCfg = {
+            xtype: 'textfield',
+            placeHolder: placeHolder,
+            listeners: {
+                focus: function (field) {
+                    var form = field.up('form');
+                    
+                    form.scroller.scrollTo({
+                        x: 0,
+                        y: field.el.getOffsetsTo(form.scrollEl)[1]
+                    });
+                }
+            }
+        };
+        
+        for (var i = 1; i <= 20; i++) {
+            items.push(Ext.apply(fieldCfg.clone(), {
+                placeHolder: 'Field ' + i
+            }));
+        }
+        
+        new Ext.form.FormPanel({
+            fullscreen: true,
+            scroll: true,
+            items: {
+                xtype: 'fieldset',
+                items: items
+            }
+        });
+    }
+});
