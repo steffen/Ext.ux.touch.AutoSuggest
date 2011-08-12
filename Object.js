cf84f9a2384ea83a85c0cef59fe3be50da775254
Steffen Hiller <s.hiller@caprireleases.com>
2011-08-11 23:00:23 -0430

    Init: Scroll field to top test.

---
+++ Object.js
@@ -0,0 +1,13 @@
+Ext.override(Object, {
+    clone: function () {
+        var obj = this;
+        
+        if (null == obj || "object" != typeof obj) return obj;
+        var copy = obj.constructor();
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+        }
+        
+        return copy;
+    }
+});
