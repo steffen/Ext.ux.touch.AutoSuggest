bb1e4b37ee4820e79eb39938b68838f30558d4d7
Steffen Hiller <s.hiller@caprireleases.com>
2011-08-22 08:27:42 -0430

    Some testing with the floating panel for the suggestion list.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+Ext.regModel('Contact', {
+    fields: ['firstName', 'lastName']
+});
+
 Ext.regApplication({
     name: 'App',
     lastYOffset: 0,
@@ -20,7 +24,51 @@ Ext.regApplication({
                     }
                     
                     this.lastYOffset = yOffset;
+                    
+                    panel = new Ext.Panel({
+                        floating: true,
+                        width: window.innerWidth - 18 * 2 - 2,
+                        height: 200,
+                        // html: 'hello',
+                        items: {
+                            xtype: 'list',
+                            itemTpl: '{firstName} {lastName}',
+                            store: new Ext.data.Store({
+                                model: 'Contact',
+                                data: [
+                                    {firstName: 'Tommy', lastName: 'Maintz'},
+                                    {firstName: 'Ed', lastName: 'Spencer'},
+                                    {firstName: 'Jamie', lastName: 'Avins'},
+                                    {firstName: 'Aaron', lastName: 'Conran'},
+                                    {firstName: 'Dave', lastName: 'Kaneda'}
+                                ]
+                            })
+                        }
+                    });
+                    
+                    // Ext.defer(function () {
+                        panel.showBy(field, false, false, false);
+                    // }, 500);
                 },
+                keyup: function (field) {
+                    // this.panel = new Ext.Panel({
+                    //     floating: true,
+                    //     width: window.innerWidth - 18 * 2,
+                    //     height: 200,
+                    //     html: 'hello'
+                    // });
+                    // 
+                    // this.panel.showBy(field, false, false, false);
+                },
+                blur: function () {
+                    // if (this.panel) {
+                    //     this.panel.destroy();
+                    // }
+                    
+                },
+                // keyup: function () {
+                //     
+                // },
                 scope: this
             }
         };
